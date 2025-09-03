a00f043618127f5eef5e9dc41e865ca24ae8ac08
DanishJaved-DJ <danishjaved03191@gmail.com>
2025-09-03 14:50:15 +0530

    fixed bug

--- server/index.js
+++ server/index.js
@@ -17,14 +17,14 @@ const app=express()
 const server=http.createServer(app)
 const io=new Server(server,{
      cors: {
-    origin: "http://localhost:5173", // production में specific domain डालना
+    origin: "https://caviar-food-delivery-app.vercel.app",
     methods: ["GET", "POST"],
     credentials: true  
   }
 })
 app.set("io", io);
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://caviar-food-delivery-app.vercel.app",
     credentials:true
 }))
 app.use(express.json())
